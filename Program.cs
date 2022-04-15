8a173513c0970ff9d0e04dbca079e7b91423ac2d
Aaron Singleterry <aaron.singleterry@claritymis.com>
2022-04-14 22:22:00 -0500

    created time span lecture namespace

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeSpanLecture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
