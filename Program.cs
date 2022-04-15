b4b72145e2ddc63c308a983bcce178bb28c84bbc
Aaron Singleterry <aaron.singleterry@claritymis.com>
2022-04-14 22:45:54 -0500

    completed lecture

--- Program.cs
+++ Program.cs
@@ -10,6 +10,34 @@ namespace TimeSpanLecture
     {
         static void Main(string[] args)
         {
+            // Creating
+
+            var timeSpan = new TimeSpan(1, 2, 3);
+            var timeSpan2 = new TimeSpan(1, 0, 0);
+            var timeSpan3 = TimeSpan.FromHours(1);
+
+            var start = DateTime.Now;
+            var end = DateTime.Now.AddMinutes(2);
+            var duration = end - start;
+            Console.WriteLine("Duration: " + duration);
+
+            // Properties
+
+            Console.WriteLine("Minutes: " + timeSpan.Minutes);
+            Console.WriteLine("Total Minutes: " + timeSpan.TotalMinutes);
+
+            // Add
+
+            Console.WriteLine("Add Example: " + timeSpan.Add(TimeSpan.FromMinutes(8)));
+            Console.WriteLine("Subtract Example: " + timeSpan.Subtract(TimeSpan.FromMinutes(8)));
+
+            // ToString
+
+            Console.WriteLine("ToString: " +  timeSpan.ToString());
+
+            // Parse
+
+            Console.WriteLine("Parse: " + TimeSpan.Parse("01:02:03"));
         }
     }
 }
